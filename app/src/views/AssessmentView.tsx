3ecb4936605aaba116e0b0237aff33dc0a7a5e92
unzuetat <unzuetat@gmail.com>
2026-03-28 10:51:17 +0100

    Update AssessmentView.tsx

--- app/src/views/AssessmentView.tsx
+++ app/src/views/AssessmentView.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect } from 'react';
-import { useTranslation } from '../../i18n';
+import { useTranslation } from '../i18n';
 import { useStore } from '../../store';
 import { DIMENSION_ORDER } from '../../data/dimensions';
 import { isAssessmentComplete } from '../../engine/scoring';
