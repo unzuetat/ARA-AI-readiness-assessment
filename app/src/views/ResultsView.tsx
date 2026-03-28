7ff25546bf670bb300c55de047b9e1d1b765bbf6
unzuetat <unzuetat@gmail.com>
2026-03-28 10:51:52 +0100

    Update ResultsView.tsx

--- app/src/views/ResultsView.tsx
+++ app/src/views/ResultsView.tsx
@@ -1,4 +1,4 @@
-import { useTranslation } from '../../i18n';
+import { useTranslation } from '../i18n';
 import { useStore } from '../../store';
 import { DIMENSION_ORDER } from '../../data/dimensions';
 import type { DimensionCode } from '../../types';
